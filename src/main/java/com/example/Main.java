ac767b16a0a91b0c092637d2c74dd96f47593f55
jin <2645123351@qq.com>
2026-01-06 10:27:13 +0800

    first2 commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
         // 查看 IntelliJ IDEA 建议如何修复。
-        System.out.printf("ranzinb!");
+        System.out.printf("ranzinb6!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
